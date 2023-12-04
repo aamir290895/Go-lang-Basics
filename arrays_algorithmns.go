42b8121bb0cc1bb080be6448b7444a410300ca94
aamir290895 <aamir.007sdl.ak@gmail.com>
2023-12-04 19:35:30 +0530

    algorithm

--- arrays_algorithmns.go
+++ arrays_algorithmns.go
@@ -2,35 +2,50 @@ package main
 
 func arrangeLowestNextToHighest(arr []int) []int {
 
+	// arr := []int {1,2,25,8,7}
+
 	s := 25
 	h := 0
+	hi := 0
+	si := 0
 
-	for _, v := range arr {
+	for i, v := range arr {
 
 		if v > h {
 
 			h = v
+			hi = i
 		}
 
 		if s > v {
 			s = v
+			si = i
 
 		}
 	}
+
+	// temp := []int{}
+
+	// for _, v := range arr {
+
+	// 	if v == h && v != s {
+	// 		temp = append(temp, v)
+	// 		temp = append(temp, s)
+
+	// 	} else if v != s {
+	// 		temp = append(temp, v)
+
+	// 	}
+	// }
+
+	arr = append(arr[:si], arr[si+1:]...)
 
 	temp := []int{}
 
-	for _, v := range arr {
+	temp = append(temp, arr[s+1:]...)
+	arr = append(arr[:hi], s)
 
-		if v == h && v != s {
-			temp = append(temp, v)
-			temp = append(temp, s)
+	arr = append(arr, temp...)
 
-		} else if v != s {
-			temp = append(temp, v)
-
-		}
-	}
-
-	return temp
+	return arr
 }
